a147db31766f4516dedf56ec5db5dedd07e081f7
Arnau Rivas <arnau.rivas.rivas@gmail.com>
2025-11-12 16:55:16 +0100

    Init Commit

---
+++ FlyMarket/FlyMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyMarketApp.swift
+//  FlyMarket
+//
+//  Created by Arnau Rivas Rivas on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FlyMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
